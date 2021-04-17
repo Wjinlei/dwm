10aac05ad3f99de3e3a352d990075bdb4b80e237
Wjinlei <1976883731@qq.com>
2021-04-18 03:53:33 +0800

    optimized

--- config.h
+++ config.h
@@ -4,11 +4,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;        /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 10;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;        /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
