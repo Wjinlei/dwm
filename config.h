ba0649bc9b155f0a47f375171d98218b6c69cb29
Wjinlei <1976883731@qq.com>
2021-04-18 14:10:14 +0800

    optimized

--- config.h
+++ config.h
@@ -2,24 +2,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;        /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;        /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;        /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 //static const char *fonts[]          = { "TerminessTTF Nerd Font Mono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
 //static const char dmenufont[]       = "TerminessTTF Nerd Font Mono:size=12";
-static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
-static const char dmenufont[]       = "UbuntuMono Nerd Font Mono:size=12";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
