dbc07cc2578a5f511bca52beb8c58193c2096743
Jerry Wang <freebufer.wang@gmail.com>
2022-04-07 22:29:33 +0800

    Apply patch -> dwm-viewontag-20210312-61bb8b2.diff

--- config.def.h
+++ config.def.h
@@ -10,9 +10,10 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 1;     /* 0 means bottom bar */
+static const int showsystray        = 1;        /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { 
     "JetBrainsMono Nerd Font Mono:size=14",
     "JoyPixels:size=14:antialias=true:autohint=true" };
--- dwm.c
+++ dwm.c
@@ -2153,6 +2153,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag && ((arg->ui & TAGMASK) != TAGMASK))
+			view(arg);
 	}
 }
 
