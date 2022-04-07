572de8d0db065a1671d379fee07fec1a83bbeae4
Jerry Wang <freebufer.wang@gmail.com>
2022-04-07 10:35:25 +0800

    Apply patch -> dwm-restartsig-20180523-6.2.diff

--- config.def.h
+++ config.def.h
@@ -98,6 +98,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
 };
 
 /* button definitions */
