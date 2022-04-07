da8dbfe21ea82c176db38de77df8cf063a79ecaa
Jerry Wang <freebufer.wang@gmail.com>
2022-04-07 10:36:45 +0800

    Apply patch -> dwm-autostart-20161205-bb3bd6f.diff

--- dwm.c
+++ dwm.c
@@ -194,6 +194,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1389,6 +1390,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/bin/dwm.script; ./dwm.autostart_blocking.sh");
+	system("cd ~/bin/dwm.script; ./dwm.autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2169,6 +2176,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	if(restart) execvp(argv[0], argv);
 	cleanup();
