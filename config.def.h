4f6978e58338479a6015f242e2044f88824f337d
Jerry Wang <freebufer.wang@gmail.com>
2022-04-08 23:40:53 +0800

    Modify border to 1 and gaps

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 20};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
