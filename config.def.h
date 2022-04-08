8f7c62b37ad4dc5a132e001028daf729246724dc
Jerry Wang <freebufer.wang@gmail.com>
2022-04-08 22:18:06 +0800

    Apply patch -> dwm-alpha-systray-6.3.diff

--- config.def.h
+++ config.def.h
@@ -8,6 +8,10 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;   /* 0 means no systray */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { 
     "JetBrainsMono Nerd Font Mono:size=14",
