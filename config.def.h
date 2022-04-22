fa514f4a1b168b76f8ef3dc30972718b3feadba4
Jerry Wang <freebufer.wang@gmail.com>
2022-04-22 23:41:15 +0800

    Modify 'gappx = 30' -> 'gappx = 15'

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 30};
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 15};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
