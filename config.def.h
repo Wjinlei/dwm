19e96f3e44704e59eacd6495a04e7a305320b175
Jerry Wang <freebufer.wang@gmail.com>
2022-04-22 23:41:33 +0800

    Modify 'gappx = 30' -> 'gappx = 15'

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 30};
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 15};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
