a8b8b08007ff37835ac2d960d75501f799ed2ed4
Jerry Wang <freebufer.wang@gmail.com>
2022-05-31 10:34:12 +0800

    Update fonts.

--- config.def.h
+++ config.def.h
@@ -15,13 +15,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = {
-    "JetBrainsMono Nerd Font Mono:size=11:antialias=true:autohint=true",
-    "Hack Nerd Font:size=11:antialias=true:autohint=true",
-    "RobotoMono Nerd Font:size=11:antialias=true:autohint=true",
-    "SauceCodePro Nerd Font Mono:size=11:antialias=true:autohint=true",
-    "FiraCode Nerd Font Mono:size=11:antialias=true:autohint=true",
-    "JoyPixels:size=8:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=11";
+    "JetBrainsMono Nerd Font Mono:size=10:antialias=true:autohint=true",
+    "RobotoMono Nerd Font:size=10:antialias=true:autohint=true",
+    "Hack Nerd Font:size=10:antialias=true:autohint=true",
+    "BlexMono Nerd Font Mono:size=10:antialias=true:autohint=true",
+    "SauceCodePro Nerd Font Mono:size=10:antialias=true:autohint=true",
+    "JoyPixels:size=9:antialias=true:autohint=true" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -30,7 +30,7 @@ static const char col_cyan[]        = "#282a36";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#282a36"  },
+	[SchemeSel]  = { col_gray4, col_cyan,  "#282a36" },
 };
 
 /* tagging */
